034c20a35e0d85399ea985fe33241f600decabba
fyhNB <1374872604@qq.com>
2022-03-01 23:34:57 +0800

    feature: 改了一些有的没的

--- Modules/Business/ToDo/ToDo/Classes/ToDoViewController.swift
+++ Modules/Business/ToDo/ToDo/Classes/ToDoViewController.swift
@@ -7,22 +7,30 @@
 
 import UIKit
 import Provider
+import RxSwift
+import RxCocoa
 
 class ToDoViewController: UIViewController {
+    
+    private var disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let testLabel = UILabel()
-        testLabel.text = "ToDo"
-        testLabel.frame = CGRect(x: 100, y: 100, width: 100, height: 40)
-        view.addSubview(testLabel)
+        let button = UIButton(type: .system)
+        button.setTitle("登录", for: .normal)
+        button.frame = CGRect(x: 100, y: 100, width: 100, height: 40)
+        view.addSubview(button)
+        
+        button.rx.tap.subscribe(onNext: { _ in
+            let accountService = PGProviderManager.shared.provider(forProtocol: { AccountProvider.self })
+            accountService?.presentLoginViewController(from: self.tabBarController!, animated: true)
+        }).disposed(by: disposeBag)
+
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        let accountService = PGProviderManager.shared.provider(forProtocol: { AccountProvider.self })
         
-        accountService?.presentLoginViewController(from: self.tabBarController!, animated: true)
     }
 
 }
